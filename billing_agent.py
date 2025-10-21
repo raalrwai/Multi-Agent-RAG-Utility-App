69baccb2eda0d97905a3e7185459f967a2e83227
Mikael Huff <mikaelkhuff@gmail.com>
2025-10-21 16:39:47 -0400

    Make test cases

--- billing_agent.py
+++ billing_agent.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import tempfile
 import zipfile
 import hashlib
@@ -77,6 +78,19 @@ func_dict = {'get_bills': get_bills, 'get_response': get_response}
 def ask_gpt(name, query, model='gpt-4.1-mini'):
 
     input_list = [{"role": "user", "content": 'find bills belonging to ' + name + ' and answer the following question:' + query}]
+    instruction = 'You are an assistant who talks with customers to help answer questions ' \
+    'about their electric usage. If the question requires information from an electric bill ' \
+    'to answer, find bills that belong to "' + name + '". Then if there are no bills with that name'\
+    'respond with only "No bills found with that name". The actual question for you to answer is "' +\
+     query +'"'
+    instruction = 'You are a friendly assistant who will help customer\'s and answer their question' \
+    'which is "' + query + '". If this question would not benefit from specific information, answer'
+    'it politely, otherwise, retrieve bill information that is under the name "' + name +\
+    '". Once you have the bills, make sure that their name is on the bill, otherwise answer with the '
+    'prhase "No bills found under that name. Refer to upload." If you do have a bill with their name, '
+    'go ahead and answer their original question with information from that bill.'
+
+    # input_list = [{"role": "user", "content": instruction}]
 
     # 2. Prompt the model with tools defined
     response = client.responses.create(
@@ -84,9 +98,15 @@ def ask_gpt(name, query, model='gpt-4.1-mini'):
         tools=tools,
         input=input_list,
     )    
-
     # Save function call outputs for subsequent requests
     input_list += response.output
+
+    original_stdout = sys.stdout
+    # print(response)
+    with open('temp.txt', 'w') as f:
+        sys.stdout = f
+        print(response)
+    sys.stdout = original_stdout
 
     for item in response.output:
         if item.type == "function_call":
@@ -105,11 +125,9 @@ def ask_gpt(name, query, model='gpt-4.1-mini'):
                 })
 
 
-    not_found_msg =  "Furthermore, if " + name + "does not show up in the bill' +\
-          print out 'No bills found with that name'."
     response = client.responses.create(
         model=model,
-        instructions="Respond only with information gotten from the get_bills tool." + not_found_msg,
+        instructions= '',
         tools=tools,
         input=input_list,
     )
@@ -124,3 +142,22 @@ def ask_gpt(name, query, model='gpt-4.1-mini'):
     # print("\n" + response.output_text)
 
     return response.output_text
+
+
+
+
+if __name__ == '__main__':
+    test_dir = os.path.join(os.getcwd(), 'tests/billing_agent/')
+    test_questions_df = pd.read_csv(os.path.join(test_dir, 'questions.csv'))
+
+    answers_path = os.path.join(test_dir, 'answers.csv')
+    with open(answers_path, 'w') as f:
+        for i in range(len(test_questions_df)):
+            name = test_questions_df.loc[i]['Name']
+            question = test_questions_df.loc[i]['Question']
+            answer = '"' + ask_gpt(name,question).replace('\n', ' ').replace('"', "'") +'"'
+            content = ','.join([name,question,answer]) + '\n'
+            f.write(content)
+            break
+
+
